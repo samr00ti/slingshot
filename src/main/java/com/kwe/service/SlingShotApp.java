78b1a61ebadecbc17758dbc706cf08c898884786
yc <yc@nogle.com>
2018-04-06 04:20:34 +0800

    Initial

---
+++ src/main/java/com/kwe/service/SlingShotApp.java
@@ -0,0 +1,13 @@
+package com.kwe.service;
+
+public class SlingShotApp {
+
+    public SlingShotApp(){
+        System.out.println ("Starting Slingshot app");
+    }
+
+    public static void main (String args[]){
+        SlingShotApp app = new SlingShotApp();
+
+    }
+}
