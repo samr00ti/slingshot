3989d6ee396a8edf7d3f2b62fb23c1dbf9bd9cf2
yc <yc@nogle.com>
2018-04-06 04:32:39 +0800

    Added submodule

--- src/main/java/com/kwe/service/SlingShotApp.java
+++ src/main/java/com/kwe/service/SlingShotApp.java
@@ -1,9 +1,18 @@
 package com.kwe.service;
 
+import com.kwe.slingshot.model.SlingShot;
+
 public class SlingShotApp {
 
     public SlingShotApp(){
         System.out.println ("Starting Slingshot app");
+
+        SlingShot slingShot = new SlingShot("branch", "Rubber 1", "rocks");
+
+        System.out.println (slingShot.toString());
+
+
+
     }
 
     public static void main (String args[]){
